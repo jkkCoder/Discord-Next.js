610884725601186ce62c90f9cd5e2274f0af8a2a
jkkCoder <jayeshk118@gmail.com>
2024-06-21 17:02:18 +0530

    prisma setup

---
+++ lib/interfaces.ts
@@ -0,0 +1,9 @@
+export interface ProfileInterface {
+    id: string;
+    userId: string;
+    name: string;
+    imageUrl: string;
+    email: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
