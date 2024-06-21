de4c9e20e9e05a32e857ac34744b669effe958c9
jkkCoder <jayeshk118@gmail.com>
2024-06-21 13:48:10 +0530

    dark theme setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <UserButton 
-      afterSignOutUrl="/"
-    />
+    <div>
+      <UserButton 
+        afterSignOutUrl="/"
+      />
+      <ModeToggle />
+    </div>
   );
 }
