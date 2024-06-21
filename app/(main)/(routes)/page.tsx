cd883614b3349471a1e8f2ccdd09606c3ef18aae
jkkCoder <jayeshk118@gmail.com>
2024-06-21 13:33:50 +0530

    clerk auth setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,6 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>this is a protected route</div>
+    <UserButton 
+      afterSignOutUrl="/"
+    />
   );
 }
