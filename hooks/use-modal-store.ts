3e0ea0de515d4978016fe775e791bd02ae3dfe9f
jkkCoder <jayeshk118@gmail.com>
2024-07-09 22:14:16 +0530

    delete and leave server'

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { Server } from "@prisma/client";
 import {create} from "zustand"
 
-export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createchannel"
+export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createchannel" | "leaveServer" | "deleteServer"
 
 interface ModalData {
     server?: Server
