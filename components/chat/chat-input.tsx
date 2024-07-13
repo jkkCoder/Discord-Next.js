36d05656d66c15dd1df3685c6ce5fa15e387cbee
jkkCoder <jayeshk118@gmail.com>
2024-07-14 02:32:00 +0530

    emoji integration

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -4,11 +4,13 @@ import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {Form, FormControl, FormField, FormItem} from "@/components/ui/form"
-import { Plus, Smile } from "lucide-react";
+import { Plus } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import axios from "axios";
 import qs from "query-string"
 import { useModal } from "@/hooks/use-modal-store";
+import { EmojiPicker } from "@/components/emoji-picker";
+import { useRouter } from "next/navigation";
 
 interface ChatInputProps {
     apiUrl: string;
@@ -29,6 +31,7 @@ export const ChatInput = ({apiUrl, query, name, type}: ChatInputProps) => {
             content: ""
         }
     })
+    const router = useRouter()
 
     const {onOpen} = useModal()
 
@@ -42,6 +45,9 @@ export const ChatInput = ({apiUrl, query, name, type}: ChatInputProps) => {
             })
 
             await axios.post(url, value)
+
+            form.reset()
+            router.refresh()
         }catch(err) {
             console.log(err)
         }
@@ -60,9 +66,9 @@ export const ChatInput = ({apiUrl, query, name, type}: ChatInputProps) => {
                                     <button type="button" onClick={() => onOpen('messageFile', {apiUrl, query})} className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"> 
                                         <Plus className="text-white dark:text-[#313338]"/>
                                     </button>
-                                    <Input {...field} placeholder={`Message ${type === 'conversation' ? name : "#" + name}`} disabled={isLoading} className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200" />
+                                    <Input {...field} placeholder={`Message ${type === 'conversation' ? name : "#" + name}`} disabled={isLoading} autoComplete="off" className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200" />
                                     <div className="absolute top-7 right-8">
-                                        <Smile />
+                                        <EmojiPicker onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`) }/>
                                     </div>
                                 </div>
                             </FormControl>
