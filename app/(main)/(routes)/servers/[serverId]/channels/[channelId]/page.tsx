94330036d08c0defe9fb16d4a698ef28e674c9f3
jkkCoder <jayeshk118@gmail.com>
2024-07-14 01:10:49 +0530

    chat component and message api

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs/server";
@@ -36,6 +37,11 @@ const ChannelIdPage = async ({params}: ChannelIdPageProps) => {
     return(
         <div className="bg-whtie dark:bg-[#313338] flex flex-col h-full">
             <ChatHeader name={channel.name} serverId={channel.serverId} type="channel"/>
+            <div className="flex-1">Future Message</div>
+            <ChatInput name={channel.name} type="channel" apiUrl="/api/socket/messages" query={{
+                channelId: channel.id,
+                serverId: channel.serverId
+            }}/>
         </div>
     )
 }
