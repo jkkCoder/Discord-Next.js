f2687f60cd3f5290ef4201fd58cd977890c8f223
jkkCoder <jayeshk118@gmail.com>
2024-07-16 00:32:50 +0530

    chat message component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
+import { ChatMessages } from "@/components/chat/chat-messages";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs/server";
@@ -24,20 +25,23 @@ const ChannelIdPage = async ({params}: ChannelIdPageProps) => {
         }
     })
 
-    const memeber = await db.member.findFirst({
+    const member = await db.member.findFirst({
         where: {
             serverId: params.serverId,
             profileId: profile.id
         }
     })
 
-    if(!channel || !memeber) {
+    if(!channel || !member) {
         redirect('/')
     }
     return(
         <div className="bg-white dark:bg-[#313338] flex flex-col h-[100vh]">
             <ChatHeader name={channel.name} serverId={channel.serverId} type="channel"/>
-            <div className="flex-1">Future Message</div>
+            <ChatMessages member={member} name={channel.name} type="channel" apiUrl="/api/messages" socketUrl="/api/socket/messages" socketQuery={{
+                    channelId: channel.id,
+                    serverId: channel.serverId
+                }} paramKey="channelId" paramValue={channel.id} chatId={channel.id}/>
             <ChatInput name={channel.name} type="channel" apiUrl="/api/socket/messages" query={{
                 channelId: channel.id,
                 serverId: channel.serverId
